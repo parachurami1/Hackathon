1fc9a0bfa34af6284a18e99a1619ca58ca87c195
parachurami1 <151645122+parachurami1@users.noreply.github.com>
2025-01-20 16:36:19 +0100

    Update conn.php

--- site/conn.php
+++ site/conn.php
@@ -5,7 +5,7 @@
     $password = getenv('DB_PASSWORD');
     $dbname = getenv('DB_NAME');
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($host, $user, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
