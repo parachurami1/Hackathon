bf77846e218e8decccd6173a4808fd7b9f29e4f0
Gerald <kailechima@gmail.com>
2025-01-21 14:26:52 +0100

    Fixed sqlite issues

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Initialize the SQLite database
+sqlite3 /var/www/html/database.sqlite < /var/www/html/init.sql
+
+# Set appropriate permissions
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+# Start Apache
+apache2-foreground
