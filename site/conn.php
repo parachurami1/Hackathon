71feee318057a3f9c8282a3a028b5a757959736e
Gerald <kailechima@gmail.com>
2025-01-21 10:00:57 +0100

    Database changes

--- site/conn.php
+++ site/conn.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
-    $servername = getenv('DB_SERVER') ?: 'db'; // Use 'db' as default, as that's the service name in Docker Compose
+    $servername = getenv('DB_SERVER') ?: 'mysql'; // Use 'db' as default, as that's the service name in Docker Compose
     $username = getenv('MYSQL_USER') ?: 'root'; // Default to root if not set
-    $password = getenv('MYSQL_PASSWORD') ?: ''; // Default password, be sure to set in the environment
+    $password = getenv('MYSQL_PASSWORD') ?: 'hackathon'; // Default password, be sure to set in the environment
     $dbname = getenv('MYSQL_DATABASE') ?: 'hackathon1'; // Default database name
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname, 3306);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
